591ac5e77eca0fc943c23339908104e776c32b41
Momchil Vachev <momchilvachev@Momchils-MacBook-Air.local>
2024-10-05 07:39:11 +0300

    first commit

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,9 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+-- Switch to the next tab
+-- Switch to the next buffer
+vim.api.nvim_set_keymap("n", "<Tab>", ":bnext<CR>", { noremap = true, silent = true })
+
+-- Switch to the previous buffer
+vim.api.nvim_set_keymap("n", "<S-Tab>", ":bprev<CR>", { noremap = true, silent = true })
